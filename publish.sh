dc334f4c5fe6ffc0b8e849751cb917f9485765e2
Remisa Yousefvand <remisa.yousefvand@gmail.com>
2020-10-03 12:14:49 +0330

    dependency update

--- publish.sh
+++ publish.sh
@@ -7,5 +7,15 @@
 # Version:       1.0.0
 # Usage:         bash publish.sh <Personal Access Token>
 
-echo "$1" | vsce login Remisa
+# This is not the best way to publish a vscode extension.
+# Compiled code should be bundled with its dependencies as a single 'js' file.
+
+rm -rf out node_modules package-lock.json
+npm i
+npm run compile
+
+rm -rf node_modules package-lock.json
+npm i --production
+
+# echo "$1" | vsce login Remisa
 vsce publish -p $1
